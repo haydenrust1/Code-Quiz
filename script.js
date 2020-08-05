21f5ad6e7d0de4896f16577016e64ee9fb27e80f
Haydenrust1 <haydenrust1@gmail.com>
2020-08-04 21:24:48 -0700

    rewrote the questions and answers

--- script.js
+++ script.js
@@ -1,28 +1,82 @@
 var beginBtn = document.querySelector('#beginBtn');
 
-var qAndA = {
-  questions: [
-    'question1',
-    'question2',
-    'question3',
-    'question4',
-    'question5',
-    'question6',
-    'question7',
-    'question8',
-    'question9',
-  ],
-  answer1: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer2: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer3: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer4: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer5: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer6: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer7: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer8: ['answer1', 'answer2', 'answer3', 'answer4'],
-  answer9: ['answer1', 'answer2', 'answer3', 'answer4'],
-};
+var qAndA = [
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+  {
+    question1: 'question',
+    answers1: ['1', '2', '3', '4'],
+    correct: 'ans',
+  },
+];
+
+for (var i = 0; i < qAndA.questions.length; i++) {
+  if (qAndA.questions[i] !== qAndA.answer) {
+  }
+}
+console.log(qAndA.questions[0]);
 
 beginBtn.addEventListener('click', function () {
   console.log('Hello!');
 });
+
+/********Garbage 
+question1: [
+  'question1',
+  'question2',
+  'question3',
+  'question4',
+  'question5',
+  'question6',
+  'question7',
+  'question8',
+  'question9',
+],
+answer1: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer2: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer3: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer4: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer5: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer6: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer7: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer8: ['answer1', 'answer2', 'answer3', 'answer4'],
+answer9: ['answer1', 'answer2', 'answer3', 'answer4'],
+***************/
