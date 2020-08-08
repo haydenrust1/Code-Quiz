6647b4f9aaa4c38e9cee69080f34e7c1cd438810
Haydenrust1 <haydenrust1@gmail.com>
2020-08-07 18:21:03 -0700

    added clock timer function

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ var timer = $('#timer');
 var line = $('#line');
 var correctAns = $('#correct');
 var wrongAns = $('#wrong');
+var question = $('#question');
+var description = $('#description');
 
-
+//Questions and Answers
 var qAndA = [
   {
     question1: 'question',
@@ -54,25 +56,50 @@ var qAndA = [
   },
 ];
 
+var totalSeconds = 90;
+var secondsElapsed = 0;
+var interval;
+
 init();
 
 //This function initializes the application upon opening and restarting the browser
-function init () {
-  timer.textContent = 0;
+function init() {
+  timer.text(0);
   correctAns.hide();
   wrongAns.hide();
   line.hide();
 }
 
+/*function for time on clock*/
+function clockTime() {
+  timer.text(totalSeconds);
+  interval = setInterval(function () {
+    totalSeconds--;
+    timer.text(totalSeconds);
+    if (totalSeconds === 0) {
+      clearInterval(interval);
+    }
+    console.log(totalSeconds);
+  }, 1000);
+}
+/*function for starting quiz on click*/
+function start() {
+  // 1. replace header with question
+  question.text();
+  // 2. hide p tag
+  description.hide();
+  // 3. change beginBtn to four buttons with answer choice
+  // 4. countdown clock
+  clockTime();
+}
+
+//for looping through questions and answers
 for (var i = 0; i < qAndA.length; i++) {
   if (null) {
   }
 }
 
-beginBtn.on('click', function () {
-  console.log('Hello!');
-});
+beginBtn.on('click', start);
 
 //TEST
 console.log(qAndA[8].answers9);
-
